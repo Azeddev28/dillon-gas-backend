3b920894cda648bd0e0ca0cd0aa59897ec8a7963
Ahsan Zahid <ahsanzahid@A006-00517.local>
2022-02-01 02:17:46 +0500

    Changes to user app

--- apis/users/admin.py
+++ apis/users/admin.py
@@ -10,24 +10,24 @@ class UserAdmin(BaseUserAdmin):
     add_form = UserCreationForm
     form = UserChangeForm
 
-    list_display = ('phone_number', 'is_staff',  'is_superuser',
+    list_display = ('email', 'is_staff',  'is_superuser',
                     'date_joined', 'last_login')
     list_filter = ('is_superuser',)
     readonly_fields = ['date_joined', 'last_login']
     fieldsets = (
         (('Credentials'), {'fields': (
-            'phone_number', 'password', 'date_joined', 'last_login', 'is_staff', 'is_superuser')}),
-        ('Personal info', {'fields': ('first_name', 'last_name', 'role')}),
+            'email', 'password', 'date_joined', 'last_login', 'is_staff', 'is_superuser')}),
+        ('Personal info', {'fields': ('first_name', 'last_name', 'role', 'phone_number')}),
         ('Groups', {'fields': ('groups',)}),
         ('Permissions', {'fields': ('user_permissions',)}),
     )
     add_fieldsets = (
         (('Credentials'), {'classes': ('wide',), 'fields': (
-            'phone_number', 'password1', 'password2'), }),
+            'email', 'phone_number', 'password1', 'password2'), }),
     )
 
-    search_fields = ('phone_number', 'name', 'phone')
-    ordering = ('phone_number',)
+    search_fields = ('email', 'name', 'phone')
+    ordering = ('email',)
     filter_horizontal = ()
 
 
--- apis/users/models.py
+++ apis/users/models.py
@@ -1,16 +1,30 @@
 import uuid
 from django.db import models
-from django.contrib.auth.models import AbstractBaseUser
-from django.contrib.auth.models import PermissionsMixin
+from django.contrib.auth.models import PermissionsMixin, AbstractBaseUser
+from django.utils.translation import gettext_lazy as _
+from django.core.validators import validate_email
+from django.contrib.auth.models import UnicodeUsernameValidator
 
 from apis.base_models import TimeStamp
 from apis.users.managers import UserManager
 
 
 class User(AbstractBaseUser, TimeStamp, PermissionsMixin):
+    username_validator = UnicodeUsernameValidator()
+    
     ROLES = (
         ('delivery_agent', 'delivery_agent'),
         ('owner', 'owner'),
+    )
+    email = models.CharField(
+        max_length=150,
+        unique=True,
+        help_text=_(
+            'Required. 150 characters or fewer. Letters, digits and @/./+/-/_ only.'),
+        validators=[username_validator, validate_email],
+        error_messages={
+            'unique': _("A user with that username already exists."),
+        },
     )
     phone_number = models.CharField(
         max_length=15,
@@ -27,13 +41,13 @@ class User(AbstractBaseUser, TimeStamp, PermissionsMixin):
     role = models.CharField(max_length=80, choices=ROLES)
     uuid = models.UUIDField(default=uuid.uuid4, editable=False, unique=True)
 
-    USERNAME_FIELD = 'phone_number'
+    USERNAME_FIELD = 'email'
     REQUIRED_FIELDS = []
 
     objects = UserManager()
 
     def __str__(self):
-        return self.phone_number
+        return self.email
 
     @property
     def full_name(self):
