eeb48f91d299e8bbac84314c8efaf346a2be91ac
Ahsan Zahid <ahsanzahid@A006-00517.local>
2022-01-31 23:33:29 +0500

    project setup for dillon gas

---
+++ config/settings/production.py
@@ -0,0 +1,12 @@
+from config.settings.base import *
+import dj_database_url
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = False
+
+ALLOWED_HOSTS = ['*']
+
+
+DATABASES = {
+    'default': dj_database_url.config(conn_max_age=600, ssl_require=True)
+}
