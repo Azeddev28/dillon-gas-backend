36eb4a7dfb7845c1904f3c2873a0301b7f7ce9f5
Ahsan Zahid <ahsanzahid@A006-00517.local>
2022-02-01 02:03:00 +0500

    New title added

--- config/urls.py
+++ config/urls.py
@@ -13,6 +13,7 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.utils.html import format_html
 from django.contrib import admin
 from django.urls import path
 from django.conf import settings
@@ -21,3 +22,7 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('admin/', admin.site.urls),
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+
+
+site_header = 'Dillon Gas'
+admin.site.site_header = format_html(site_header)
