e635cb5e197a4c6e854b5f1897852ef9dff3e539
Ahsan Zahid <ahsanzahid@A006-00517.local>
2022-02-13 19:15:57 +0500

    Email port added in env

--- config/settings/base.py
+++ config/settings/base.py
@@ -156,6 +156,6 @@ STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_USE_TLS = True
-EMAIL_PORT = 587
+EMAIL_PORT = env("EMAIL_PORT")
 EMAIL_HOST_USER = env("EMAIL_HOST_USER")
 EMAIL_HOST_PASSWORD = env("EMAIL_HOST_PASSWORD")
