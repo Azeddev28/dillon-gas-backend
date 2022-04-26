9fc929e49c852f9b8a9ca6b4b77c3c65a040d681
Ahsan Zahid <ahsanzahid@A006-00517.local>
2022-04-26 23:18:05 +0500

    Stations app added

--- apis/stations/admin.py
+++ apis/stations/admin.py
@@ -1,17 +1,7 @@
 from django.contrib import admin
 
-from apis.stations.models import GasStation, Inventory
+from apis.stations.models import Station, StationInventory
 
 
-class GasStationAdmin(admin.ModelAdmin):
-
-    list_display = ('station_id', 'city',  'owner',)
-
-
-class InventoryAdmin(admin.ModelAdmin):
-
-    list_display = ('item', 'price',  'quantity', 'station')
-
-
-admin.site.register(GasStation, GasStationAdmin)
-admin.site.register(Inventory, InventoryAdmin)
+admin.site.register(Station)
+admin.site.register(StationInventory)
--- apis/stations/models.py
+++ apis/stations/models.py
@@ -1,27 +1,32 @@
 from django.db import models
 from django.contrib.auth import get_user_model
 
-from apis.base_models import TimeStamp
+from apis.base_models import BaseModel
 
 User = get_user_model()
 
 
-class GasStation(TimeStamp):
+class Station(BaseModel):
     location = models.TextField()
     city = models.TextField()
     station_id = models.CharField(max_length=200)
     owner = models.ForeignKey(
         User, on_delete=models.CASCADE, related_name='gas_station')
 
+    class Meta:
+        db_table = "stations"
+
     def __str__(self):
         return self.station_id
 
-class Inventory(TimeStamp):
+
+class StationInventory(BaseModel):
     item = models.CharField(max_length=100)
     quantity = models.IntegerField()
     price = models.FloatField()
-    station = models.ForeignKey(
-        GasStation, on_delete=models.CASCADE, related_name='inventory')
+    station = models.ForeignKey(Station, on_delete=models.CASCADE, related_name='inventory')
+    tax = models.FloatField(null=True, blank=True)
 
     class Meta:
+        db_table = 'inventory'
         verbose_name_plural = 'inventories'
