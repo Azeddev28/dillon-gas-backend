dca9c9fe09e924ae2ff104ee9938a0d2e0c3f041
Ahsan Zahid <ahsanzahid@A006-00517.local>
2022-05-10 00:05:43 +0500

    Registion fields modified, serializer excluded registration fields

--- apis/users/serializers/registration.py
+++ apis/users/serializers/registration.py
@@ -10,8 +10,8 @@ from apis.users.utils.messages import PASSWORD_MATCH_ERROR_MESSAGE
 User = get_user_model()
 
 
-def _create_device_info(user, device):
-    UserDevice.objects.create(user=user, device_id=device.get('device_id'))
+def _create_device_info(user, device_id):
+    UserDevice.objects.create(user=user, device_id=device_id)
 
 
 def _create_user(validated_data):
@@ -39,32 +39,21 @@ class DeviceSerializer(serializers.ModelSerializer):
 class RegisterSerializer(serializers.ModelSerializer):
     """Serializer for User Registration"""
     password = serializers.CharField(style={'input_type': 'password'}, write_only=True)
-    password2 = serializers.CharField(style={'input_type': 'password'}, write_only=True)
-    device = DeviceSerializer()
+    device_id = serializers.CharField(write_only=True)
     is_active = serializers.BooleanField(default=False)
     verification_code = serializers.CharField(default=EmailVerificationService.generate_verification_code())
 
     class Meta:
         model = User
         fields = ['email', 'first_name', 'last_name',
-                  'role', 'password2', 'password',
-                  'created_at', 'device',
+                  'role', 'password', 'created_at', 'device_id',
                   'is_active', 'verification_code']
         read_only_fields = ['created_at', 'updated_at']
 
-    def validate(self, attrs):
-        validated_data = super().validate(attrs)
-        password = attrs.get('password')
-        password2 = attrs.get('password2')
-        if password != password2:
-            raise serializers.ValidationError({'password': PASSWORD_MATCH_ERROR_MESSAGE}, code='registration')
-
-        return validated_data
-
     @transaction.atomic
     def create(self, validated_data):
-        device = validated_data.pop('device', None)
+        device_id = validated_data.pop('device_id', None)
         user = _create_user(validated_data)
-        _create_device_info(user, device)
+        _create_device_info(user, device_id)
         _verify_user(user.email, validated_data.get('verification_code'))
         return user
