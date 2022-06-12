dfd731f34a1b21f31f35c21f5a39d3f80d884c39
Ahsan Zahid <ahsanzahid@A006-00517.local>
2022-06-12 17:11:37 +0500

    Heroku setup

--- config/settings/production.py
+++ config/settings/production.py
@@ -1,46 +1,16 @@
 from config.settings.base import *
 
+import dj_database_url
+
+
+prod_db = dj_database_url.config(conn_max_age=500)
+
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['161.35.189.93', ]
-
-# Database Credentials
-
-DATABASE_NAME = env('DATABASE_NAME')
-DATABASE_USER = env('DATABASE_USER')
-DATABASE_PASSWORD = env('DATABASE_PASSWORD')
-DATABASE_HOST = env('DATABASE_HOST')
-DATABASE_PORT = env('DATABASE_PORT')
-
-# Database
-# https://docs.djangoproject.com/en/2.2/ref/settings/#databases
+# CHANGE THIS ACCORDINLY TO THE DOMAIN SELECTED
+ALLOWED_HOSTS = ['dillon-gas-app.herokuapp.com']
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': DATABASE_NAME,
-        'USER': DATABASE_USER,
-        'PASSWORD': DATABASE_PASSWORD,
-        'HOST': DATABASE_HOST,
-        'PORT': DATABASE_PORT,
-    }
+    'default': prod_db
 }
-
-AWS_ACCESS_KEY_ID = env('AWS_ACCESS_KEY_ID')
-AWS_SECRET_ACCESS_KEY = env('AWS_SECRET_ACCESS_KEY')
-AWS_STORAGE_BUCKET_NAME = env('AWS_STORAGE_BUCKET_NAME')
-AWS_S3_ENDPOINT_URL = env('AWS_S3_ENDPOINT_URL')
-AWS_S3_OBJECT_PARAMETERS = {
-    'CacheControl': 'max-age=86400',
-}
-AWS_LOCATION = env('AWS_LOCATION')
-
-STATIC_URL = f'https://{AWS_S3_ENDPOINT_URL}/{AWS_LOCATION}/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
-
-MEDIA_URL = f'https://{AWS_S3_ENDPOINT_URL}/{AWS_LOCATION}/'
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
-
-STATICFILES_STORAGE = 'config.storages.StaticStorage'
-DEFAULT_FILE_STORAGE = 'config.storages.MediaStorage'
