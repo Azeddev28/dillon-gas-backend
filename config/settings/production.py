7b7e0222717279e37d1ca4f2dea070e73f937142
Ahsan Zahid <ahsanzahid@A006-00517.local>
2022-06-12 17:41:23 +0500

    Production check

--- config/settings/production.py
+++ config/settings/production.py
@@ -8,6 +8,7 @@ prod_db = dj_database_url.config(conn_max_age=500)
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
+
 # CHANGE THIS ACCORDINLY TO THE DOMAIN SELECTED
 ALLOWED_HOSTS = ['dillon-gas-app.herokuapp.com']
 
