a66e476805bbd76dfcb4735d6a4fa164fc537056
Ahsan Zahid <ahsanzahid@A006-00517.local>
2022-06-20 21:29:58 +0500

    Virtual account api added

--- apis/wallets/urls.py
+++ apis/wallets/urls.py
@@ -1,11 +1,14 @@
-# from apis.wallets.views import PaymentCardModelViewset
+from django.urls import path
 
 from rest_framework.routers import SimpleRouter
+
+from apis.wallets.bindings import virtual_account_retrieve_api_view
 
 router = SimpleRouter()
 # router.register('payment-card', PaymentCardModelViewset, basename='orders')
 
 
 urlpatterns = [
+    path('virtual-account/', virtual_account_retrieve_api_view, name='virtual-account')
     # *router.urls
 ]
