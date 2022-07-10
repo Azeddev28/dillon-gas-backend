f0b571bb6cb5e810312c64c0c4e3a123d0d1fad0
Ahsan Zahid <ahsanzahid@A006-00517.local>
2022-07-11 00:22:44 +0500

    Item qualification fixes

--- apis/inventory/admin.py
+++ apis/inventory/admin.py
@@ -1,8 +1,7 @@
 from django.contrib import admin
 
-from apis.inventory.models import Category, Item, ItemQualification
+from apis.inventory.models import Category, Item
 
 
 admin.site.register(Category)
 admin.site.register(Item)
-admin.site.register(ItemQualification)
