e9b8f5eeaaaefb373e3e3339a7c70eccad123e88
Ahsan Zahid <ahsanzahid@A006-00517.local>
2022-07-11 00:25:41 +0500

    Item qualification list view removed

--- apis/stations/bindings.py
+++ apis/stations/bindings.py
@@ -1,7 +1,5 @@
-from apis.stations.views.inventory import (InventoryListAPIView, ItemDetailsRetrieveAPIView,
-                                           ItemQualificationsListAPIView)
+from apis.stations.views.inventory import (InventoryListAPIView, ItemDetailsRetrieveAPIView)
 
 
 inventory_list_view = InventoryListAPIView.as_view()
 item_details_view = ItemDetailsRetrieveAPIView.as_view()
-item_qualifications_view = ItemQualificationsListAPIView.as_view()
--- apis/stations/urls.py
+++ apis/stations/urls.py
@@ -1,10 +1,9 @@
 from django.urls import path
 
-from apis.stations.bindings import inventory_list_view, item_details_view, item_qualifications_view
+from apis.stations.bindings import inventory_list_view, item_details_view
 
 
 urlpatterns = [
     path('inventory/', inventory_list_view, name='inventory'),
     path('inventory/item-details/<uuid:uuid>/', item_details_view, name='item-details'),
-    path('inventory/item-qualifications/', item_qualifications_view, name='item-qualifications')
 ]
