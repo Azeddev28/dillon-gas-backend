348a6b78b59e17c24271efc3c620996691716f21
Ahsan Zahid <ahsanzahid@A006-00517.local>
2022-08-14 18:45:34 +0500

    Order admin display enabled

--- apis/orders/admin.py
+++ apis/orders/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
 
-# Register your models here.
+from apis.orders.models import Order, OrderItem
+
+
+class OrderItemInline(admin.StackedInline):
+    model = OrderItem
+    fields = ['name', 'uuid']
+
+
+class OrderAdmin(admin.ModelAdmin):
+    list_display = ['uuid', 'order_key', 'created_at']
+    class Meta:
+        model = Order
+
+
+admin.site.register(Order, OrderAdmin)
