9217f985cd2d9a3c3b5b1e98e74223f8f09927c3
Ahsan Zahid <ahsanzahid@A006-00517.local>
2022-09-07 14:31:40 +0500

    Category ordering, selected address in delivery info

--- apis/delivery_management/serializers.py
+++ apis/delivery_management/serializers.py
@@ -2,9 +2,20 @@ from rest_framework import serializers
 from cities_light.models import City, Region
 
 from apis.delivery_management.models import DeliveryInfo
+from apis.users.serializers.customer import CustomerAddressSerializer
 
 
 class DeliveryInfoSerializer(serializers.ModelSerializer):
+    customer_address = serializers.SerializerMethodField()
+
+    def get_customer_address(self, instance):
+        selected_address = self.context.get('request').user.selected_address
+        if not selected_address:
+            return
+
+        customer_address_serializer = CustomerAddressSerializer(selected_address)
+        return customer_address_serializer.data
+
     class Meta:
         model = DeliveryInfo
         exclude = ['id', 'created_at', 'updated_at', 'is_active']
--- apis/inventory/views/category.py
+++ apis/inventory/views/category.py
@@ -1,3 +1,5 @@
+from django.db.models import Case, When
+
 from rest_framework.generics import ListAPIView
 from rest_framework.permissions import IsAuthenticated
 
@@ -7,5 +9,7 @@ from apis.inventory.serializers.category import CategorySerializer
 
 class CategoryListAPIView(ListAPIView):
     serializer_class = CategorySerializer
-    queryset = Category.objects.all()
+    queryset = Category.objects.all().order_by(
+        Case(When(name="All", then=0), default=1)
+    )
     permission_classes = [IsAuthenticated]
