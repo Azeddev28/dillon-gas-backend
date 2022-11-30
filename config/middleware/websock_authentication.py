c8b7bd36130513f86c6640098d1c2c7b6ee5f8e8
Ahsan Zahid <ahsanzahid@Ahsan-Pioneer.local>
2022-11-30 23:13:55 +0500

    websockets integrated

--- config/middleware/websock_authentication.py
+++ config/middleware/websock_authentication.py
@@ -1,10 +1,10 @@
 from urllib.parse import parse_qs
 from django.db import close_old_connections
 from django.contrib.auth import get_user_model
-from django_cognito_jwt.validator import TokenError
 from django.contrib.auth.models import AnonymousUser
 
-from rest_framework_simplejwt.backends import TokenBackend
+from rest_framework_simplejwt.authentication import JWTAuthentication
+
 from channels.middleware import BaseMiddleware
 from channels.auth import AuthMiddlewareStack
 from channels.db import database_sync_to_async
@@ -16,9 +16,9 @@ User = get_user_model()
 @database_sync_to_async
 def get_user(token_key):
     try:
-
-        valid_data = TokenBackend(algorithm='HS256').decode(token_key, verify=True)
-        user = valid_data['user']
+        token_auth = JWTAuthentication()
+        token = token_auth.get_validated_token(token_key)
+        user = token_auth.get_user(token)
         return user
     except Exception as e:
         return AnonymousUser()
@@ -32,9 +32,8 @@ class JWTAuthMiddleware(BaseMiddleware):
         close_old_connections()
 
         try:
-            token_key = parse_qs(scope['query_string'].decode('utf-8'))['token'][0].split()[1]
-            # jwt_payload = self.token_validator.validate(token_key)
-        except (TokenError, Exception) as e:
+            token_key = parse_qs(scope['query_string'].decode('utf-8'))['token'][0]
+        except Exception as e:
             raise ValueError(
                 f'Exception occurred while authenticating Websocket request {e}')
 
